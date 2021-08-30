846a1265762de69926e2fa7694afc994e497aa7d
Ilya Lovriakov <ilya.lovriakov@booking.com>
2021-08-30 11:01:50 +0200

    switch to typescript, add linting

--- index.ts
+++ index.ts
@@ -1,15 +1,9 @@
-/*jslint node: true, nomen: true, indent: 2, vars: true, regexp: true */
+export type HighlightSQLOptions = {
+  capitalizeKeywords?: boolean;
+}
 
-'use strict';
-
-function highlightSQL(text, options) {
-  if(typeof options === 'undefined'){
-    options = {
-      capitalizeKeywords:false
-    }
-  }
-
-  var keyWords = [
+export default function highlightSQL(text: string, { capitalizeKeywords = false }: HighlightSQLOptions = { }): string {
+  const keyWords = [
     'PRAGMA', 'CREATE', 'EXISTS', 'INTEGER', 'PRIMARY', 'VARCHAR',
     'DATETIME', 'NULL', 'REFERENCES', 'AND', 'AS', 'ASC', 'INDEX_LIST',
     'BETWEEN', 'BY', 'CASE', 'CURRENT_DATE', 'CURRENT_TIME', 'DELETE',
@@ -20,24 +14,29 @@ function highlightSQL(text, options) {
     'RIGHT', 'SELECT', 'SET', 'TABLE', 'THEN', 'TO', 'TRUE', 'UPDATE',
     'VALUES', 'WHEN', 'WHERE', 'UNSIGNED', 'CASCADE', 'UNIQUE', 'DEFAULT',
     'ENGINE', 'TEXT', 'auto_increment', 'SHOW', 'INDEX'
-  ],
-    len = keyWords.length,
-    i;
+  ];
+  const len = keyWords.length;
 
-  //adding lowercase keyword support
-  for (i = 0; i < len; i += 1) {
-    keyWords.push(keyWords[i].toLowerCase());
+  // storing LC to UC map
+  const keywordsMap: {[key: string]: string} = {};
+
+  // adding lowercase keyword support
+  for (let i = 0; i < len; i += 1) {
+    const lcKeyWord = keyWords[i].toLowerCase();
+    keywordsMap[lcKeyWord] = keyWords[i];
+    keyWords.push(lcKeyWord)
   }
 
-  var regEx;
-  var clearStyle = '\x1b[0m';
-  var red = '\x1b[31m';
-  var green = '\x1b[32m';
-  var yellow = '\x1b[33m';
-  var magenta = '\x1b[35m';
+  let regEx;
+  const clearStyle = '\x1b[0m';
+  const red = '\x1b[31m';
+  const green = '\x1b[32m';
+  const yellow = '\x1b[33m';
+  const magenta = '\x1b[35m';
+
   // just store original
   // to  compare for
-  var newText = text;
+  let newText = text;
 
   // regex time
   // looking fo defaults
@@ -56,21 +55,19 @@ function highlightSQL(text, options) {
   newText = newText.replace(/(\w*?)\(/g, red + '$1' + clearStyle + '(');
 
   //brackets - same as special chars
+  //eslint-disable-next-line
   newText = newText.replace(/([\(\)])/g, yellow + '$1' + clearStyle);
 
   //reserved mysql keywords
-  for (i = 0; i < keyWords.length; i += 1) {
+  for (let i = 0; i < keyWords.length; i += 1) {
     //regex pattern will be formulated based on the array values surrounded by word boundaries. since the replace function does not accept a string as a regex pattern, we will use a regex object this time
     regEx = new RegExp('\\b' + keyWords[i] + '\\b', 'g');
-    var keyWord = keyWords[i]
-    if(options.capitalizeKeywords){
-      keyWord = keyWords[i].toUpperCase()
+    let keyWord = keyWords[i]
+    if (capitalizeKeywords && keywordsMap[keyWord]) {
+      keyWord = keywordsMap[keyWord]
     }
     newText = newText.replace(regEx, magenta + keyWord + clearStyle);
   }
 
   return newText;
-
-};
-
-module.exports = highlightSQL;
+}
